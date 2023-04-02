92ffca2c4f2ea0bb0703d7eceedd1d3e045b089b
cnestordev <ncervan87@gmail.com>
2023-04-01 18:52:56 -0700

    updates proxy configuration

--- client/src/proxySetup.js
+++ client/src/proxySetup.js
@@ -1,10 +1,12 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = function (app) {
+  const target = "http://localhost:5000";
+
   app.use(
     ["/api", "/user", "/auth"],
     proxy({
-      target: "http://localhost:5000",
+      target,
       changeOrigin: true,
     })
   );
